cfb85caa5ee93e3bc05ab61aa388320f0bda2a02
Thomas Doukas <tom.doukas7@gmail.com>
2020-06-15 23:10:51 +0300

    fixxed add product form

--- src/containers/Products/AddProduct/AddProduct.js
+++ src/containers/Products/AddProduct/AddProduct.js
@@ -15,6 +15,7 @@ class AddProduct extends Component {
             price: null,
             name: null,
             brand_name: null,
+            category_id: 0,
             first_transaction: null
         }
     }
@@ -29,6 +30,8 @@ class AddProduct extends Component {
             alert("Oops! To create a product you must specify all parameters.");
         else if (data.price <= 0)
             alert("Oops! Invalid product price");
+        else if ( data.category_id === 0)
+            alert("Oops! You have not selected a category")
         else {
             axios.post('/products', data)
                 .then(res => {
@@ -70,6 +73,26 @@ class AddProduct extends Component {
                             <label>Brand Name: </label>
                             <br />
                             <input type="text" placeholder={"example brand"} name="brand_name" onChange={this.changeHandler} />
+                        </div>
+
+                        <div>
+                            <label>Category: </label>
+                            <br />
+                            <input type="text" placeholder={"example category"} name="category_id" onChange={this.changeHandler} />
+                        </div>
+
+                        <div>
+                            <label>Product Category: </label>
+                            <br />
+                            <select name="category_id" onChange={this.changeHandler} value={0}>
+                                <option value={0}> NoInfo </option>
+                                <option value={2}> Dairy and Frozen </option>
+                                <option value={3}> Drinks </option>
+                                <option value={1}> Fresh </option>
+                                <option value={5}> Household </option>
+                                <option value={4}> Personal </option>
+                                <option value={6}> Pet </option>
+                            </select>
                         </div>
 
                         <div>
